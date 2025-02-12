48ad5c8d5402cb3c074ddcd37ee11535aee1820d
MdImran-Hossain <mdimranhossain.web@gamil.com>
2025-02-12 23:35:56 +0600

    first commit

---
+++ src/pages/Registration.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Registration = () => {
+  return (
+    <>
+      <section>
+        <div>
+            <div>
+                <div>
+                <h1 className='text-[34px] font-bold text-font_color font-Nunito'>Get started with easily register</h1>
+                <p className='text-[20px] font-normal font-Nunito text-black opacity-(0.47)'>Free register and you can enjoy it</p>
+                </div>
+            </div>
+            <div>
+
+            </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Registration
